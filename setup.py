0cd38c11fc62ef72908ac08f4a9e0f0ebaed042a
kaushi <kaushika@silverlineit.co>
2025-02-10 16:39:21 +0530

    change the setup.py  file

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_requirements(file_path:str)->List[str]:
 
 setup(
     name="libfinder",
-    version="0.0.1",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=get_requirements("requirements.txt"),
     author="Kaushi Gihan",
