40e3329ed3d5a423a7d4ce38c63a419358fb57e0
kaushi <kaushika@silverlineit.co>
2025-02-10 12:31:02 +0530

    change the test_libfinder.py  file

---
+++ test/test_libfinder.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+from libfinder import libraryfinder,get_lib_name,get_lib_version
+
+def test_libfinder():
+    
+    print("Library name : ",get_lib_name())
+    print("Library versions : ",get_lib_version)
+
+
+    lib=libraryfinder()
+    #Get the lib versions
+    print("libraryfinder class library versions :",lib.get_lib_version())
+    #save the lib in txt file
+    lib.to_txt("requirement3.txt")
+
+
+
