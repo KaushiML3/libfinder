856962ccff9dc9b5b5a9f052cc975c86984d494b
kaushi <kaushika@silverlineit.co>
2025-02-11 17:58:01 +0530

    add the test_pypi to the setup file

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def get_requirements(file_path:str)->List[str]:
 #"License :: OSI Approved :: MIT License",
 setup(
     name="libfinder",
-    version="0.1.2",
+    version="0.1.3",
     packages=["libfinder"],
     install_requires=get_requirements("requirements.txt"),
     author="Kaushi Gihan",
